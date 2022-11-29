9cfb143de0b3cec1d6d8c86547de1c86fbb914e9
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2022-11-28 17:45:04 -0700

    fs to read db and add it to an array

--- server.js
+++ server.js
@@ -27,8 +27,16 @@ app.post("/api/notes", (req, res) => {
   console.log(req.body);
   noteData = req.body;
   //read the DB file
-
-  fs.appendFile("./db/db.json", JSON.stringify(noteData), function (err) {
+  fs.readFile("./db/db.json", "utf8", (error, data) => {
+    if (!error) {
+      array = [JSON.parse(data)];
+      array.push(noteData);
+      console.log(array);
+    } else {
+      console.log(error);
+    }
+  });
+  fs.appendFile("./db/db.json", JSON.stringify(array), function (err) {
     if (err) throw err;
     console.log("Saved!");
   });
