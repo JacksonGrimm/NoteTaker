55926889211b76c6de09e2a974c453963beb0cee
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2022-11-28 17:51:16 -0700

    takes array from json and adds new note then appends file

--- server.js
+++ server.js
@@ -27,18 +27,19 @@ app.post("/api/notes", (req, res) => {
   console.log(req.body);
   noteData = req.body;
   //read the DB file
+  let array = [];
   fs.readFile("./db/db.json", "utf8", (error, data) => {
     if (!error) {
       array = [JSON.parse(data)];
       array.push(noteData);
       console.log(array);
+      fs.appendFile("./db/db.json", JSON.stringify(array), function (err) {
+        if (err) throw err;
+        console.log("Saved!");
+      });
     } else {
       console.log(error);
     }
-  });
-  fs.appendFile("./db/db.json", JSON.stringify(array), function (err) {
-    if (err) throw err;
-    console.log("Saved!");
   });
 });
 
