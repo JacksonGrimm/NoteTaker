9fad7cd92f212e5ff23efb41773d68d9fd63a497
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2022-12-01 10:38:47 -0700

    added env port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const util = require("util");
 const path = require("path");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.static("public"));
